a9b890781ca3bd6ecaca0b72ac5302fe568f665b
Rutuja Shah <rutu.shah.26@gmail.com>
2020-03-10 15:56:16 -0700

    Added Proc MSR

--- CMPE-283/Assignment-1/cmpe283-1.c
+++ CMPE-283/Assignment-1/cmpe283-1.c
@@ -12,6 +12,7 @@
  * See SDM volume 4, section 2.1
  */
 #define IA32_VMX_PINBASED_CTLS	     0x481
+#define IA32_VMX_PROCBASED_CTLS      0x482
 #define IA32_VMX_PROCBASED_CTLS2     0x48B
 #define IA32_VMX_EXIT_CTLS           0x483
 
@@ -90,6 +91,31 @@ struct capability_info exit_ctls[13] =
 	{ 28, "Load CET state" }
 };
 
+struct capability_info procbased[21] =
+{
+	{ 2, "Interrupt-window exiting"},
+	{ 3, "Use TSC offsetting" },
+	{ 7, "HLT Exiting" },
+	{ 9, "INVLPG Exiting" },
+	{ 10, "MWAIT Exiting" },
+	{ 11, "RDPMC Exiting" },
+	{ 12, "RDTSC Exiting" },
+	{ 15, "CR3-load Exiting" },
+	{ 16, "CR3-store Exiting" },
+	{ 19, "CR8-load Exiting" },
+	{ 20, "CR8-store Exiting" },
+	{ 21, "Use TPR shadow" },
+	{ 22, "NMI-window exiting" },
+	{ 23, "MOV-DR exiting" },
+	{ 24, "Unconditional I/O exiting" },
+	{ 25, "Use I/O bitmaps" },
+	{ 27, "Monitor trap flag" },
+	{ 28, "Use MSR bitmaps" },
+	{ 29, "MONITOR exiting" },
+	{ 30, "PAUSE exiting" },
+      	{ 31, "Activate Secondary Controls" }
+};
+
 /*
  * report_capability
  *
@@ -137,7 +163,13 @@ detect_vmx_features(void)
 	pr_info("Pinbased Controls MSR: 0x%llx\n",
 		(uint64_t)(lo | (uint64_t)hi << 32));
 	report_capability(pinbased, 5, lo, hi);
+	pr_info("------------------------------\n");
 
+	/* Procbased controls */
+	rdmsr(IA32_VMX_PROCBASED_CTLS, lo, hi);
+	pr_info("Procbased Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(procbased, 21, lo, hi);
 	pr_info("------------------------------\n");
 
 	/* Proc based controls2 */
@@ -145,12 +177,11 @@ detect_vmx_features(void)
 	pr_info("Proc based Controls2 MSR: 0x%llx\n",
 		(uint64_t)(lo | (uint64_t)hi << 32));
 	report_capability(proc_based2, 27, lo, hi);
-
 	pr_info("------------------------------\n");
 
-	/* Proc based controls2 */
+	/* Exit controls */
 	rdmsr(IA32_VMX_EXIT_CTLS, lo, hi);
-	pr_info("Exit based Controls2 MSR: 0x%llx\n",
+	pr_info("Exit based Controls MSR: 0x%llx\n",
 		(uint64_t)(lo | (uint64_t)hi << 32));
 	report_capability(exit_ctls, 13, lo, hi);
 }
